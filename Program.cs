7c82909f46c87c1cf30cd4ab3923c4335c4e1110
alexonplus <oleks9185@gmail.com>
2025-11-07 14:14:21 +0100

    Added spectre.console Added a UI folder (for design) Created a LoadInfo class that displays loading progress when opening the application -Added a loading screen

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
-﻿namespace ShapeUp
+﻿using Test;
+
+namespace ShapeUp
 {
     internal class Program
     {
         static void Main(string[] args)
+
         {
+            ProgressDemo.Run();
             Console.WriteLine("Hello,!");
         }
     }
