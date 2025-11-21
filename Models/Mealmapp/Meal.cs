37d6b5b3ba90cfdb82142565a4108bce34e4a277
Henry0811111 <Henry0811111>
2025-11-21 11:12:10 +0100

    Skapae kost och meny

---
+++ Models/Mealmapp/Meal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeUp.Models.Meal
+{
+    public class Meal
+    {
+        public int Id { get; set; }
+        public string MealName { get; set; }
+        public List<Ingredient> Ingredients { get; set; } 
+        public int TotalCalories { get; set; }
+    }
+
+        public class Ingredient
+        {
+            public string Name { get; set; }
+            public int Calories { get; set; }
+        }
+    }
